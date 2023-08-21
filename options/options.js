182328d5da3b50df2d0c50af6acacdd10ff0d08c
Crowdbotics <engineering@crowdbotics.com>
2023-08-21 23:49:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-poetry-d-110201",
+  url: "https://billowing-poetry-d-110201.botics.co",
+  api: "https://billowing-poetry-d-110201.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
